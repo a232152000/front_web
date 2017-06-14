ea508ad2724b233f6923e9f18b5b7422d7a7535e
a232152000 <a232152000@gmail.com>
2017-06-14 12:14:20 +0800

    重大更新 1.更名index.html->index.php 2.index.php加入即時分析功能 3.index.php加入fb_token輸入功能,不用再進去get_fanpage_posts.py修改fb_token 4.get_fanpage_posts.py的fb_token改成由index.php傳入

---
+++ get_fanpage_posts.py
@@ -0,0 +1,107 @@
+import requests # pip install requests
+import facebook
+import datetime
+import json
+import sys
+
+def getFanPagePosts(fanPage, getTime):
+    posts=[]
+    print(fanPage)
+    getPosts(fanPage, posts, graphApi.get_object(fanPage, fields="posts{created_time,message,id}")["posts"], getTime)
+
+def getPosts(fanPage, posts, postsInfo, getTime):
+    posts += [posts
+             for posts in postsInfo["data"]
+             if "data" in postsInfo]
+    print(len(posts))
+    inTimePosts = [post#["created_time"]
+                 for post in posts
+                 if datetime.datetime.strptime(post["created_time"], "%Y-%m-%dT%H:%M:%S+%f")>=getTime]
+    print(len(inTimePosts))
+    if len(inTimePosts) == len(posts):
+        if "next" in postsInfo["paging"]:
+            getPosts(fanPage, posts, requests.get(postsInfo["paging"]["next"]).json(), getTime)
+    else:
+        fanPagePosts[fanPage] = inTimePosts
+
+
+
+ACCESS_TOKEN= sys.argv[1]
+broadCasters=["SenYeYeZi", "6tantan", "LeggyReki", "rdhose", "LNGniaws", "chrisop999", "yuniko0720", "WannaSinging", "opchanneltw", "Heavenknowslol"]
+fanPagePosts={}
+
+print("---------------")
+print("get GraphApi")
+graphApi = facebook.GraphAPI(ACCESS_TOKEN)
+print("---------------")
+print("get fanpage post")
+for name in broadCasters:
+    getFanPagePosts(name, datetime.datetime(2017, 5, 1))
+
+def getGameList(gamefile):
+    with open(gamefile, "r") as f:
+        allGame = f.readlines()
+        gameList=[]
+        for game in allGame:
+            gameList.append(game.strip('\n').split('|'))
+    f.close()
+    return gameList
+
+gameList=[]
+gameList=getGameList("gameList.txt")
+
+def Game(people_post,game_list):
+    people_game = {}
+
+    for j in game_list:
+        people_game[j[0]] = 0
+
+    for i in range(len(people_post)):
+        for j in game_list:
+            for k in j:
+               if(k in people_post[i]):
+                   people_game[j[0]] =people_game[j[0]] + 1
+                   break
+    return people_game
+
+people = {}
+for name in broadCasters:
+    message = [posts['message'] for posts in fanPagePosts[name] if 'message' in posts]
+    game_count = Game(message,gameList)
+    people[name] = game_count
+
+def Game_People_Json(people,game_list,broadCasters):
+    game_people = {}
+    for i in game_list:
+        garbage = {}
+        for j in broadCasters:
+            garbage[j] = people[j].get(i[0])
+            #print(garbage[j])
+
+            #print(game_people[i[0]])
+        gg = sorted(garbage.items(), key=lambda d:d[1], reverse = True)
+        game_people[i[0]] = gg
+
+    return game_people
+game_people_json={}
+game_people_json = Game_People_Json(people,gameList,broadCasters)
+with open('FanPage.json', 'w') as fp:
+        json.dump(game_people_json, fp)
+#print(json.dump(game_people_json))
+#匯出封面+頭圖
+Pic_CovUrl={}
+def getFanPagePictureAndCoverUrl(fanPage):
+    creatTime = [posts['created_time'] for posts in fanPagePosts[fanPage] if 'created_time' in posts]
+    newPost = datetime.datetime.strptime(creatTime[0], "%Y-%m-%dT%H:%M:%S+%f")
+    oldPost = datetime.datetime.strptime(creatTime[-1], "%Y-%m-%dT%H:%M:%S+%f")
+    postTime = str(newPost.year)+"年"+str(newPost.month)+"月"+str(newPost.day)+"日 "+str(newPost.hour)+"點"+str(newPost.minute)+"分 到 "+str(oldPost.year)+"年"+str(oldPost.month)+"月"+str(oldPost.day)+"日 "+str(oldPost.hour)+"點"+str(oldPost.minute)+"分"
+    url={}
+    url["picture"] = "http://graph.facebook.com/" + fanPage + "/picture?type=large"
+    url["cover"] = graphApi.get_object(fanPage, fields="cover")["cover"]["source"]
+    url["post_time"] = postTime
+    Pic_CovUrl[fanPage] = url
+for name in broadCasters:
+    getFanPagePictureAndCoverUrl(name)
+
+with open('FanPagePictureAndCoverUrl.json', 'w') as fp:
+        json.dump(Pic_CovUrl, fp)
---
+++ index.php
@@ -0,0 +1,212 @@
+<?php
+  if(@isset($_POST[fb_token]))
+  {
+    $fb_token = $_POST[fb_token];
+    echo("Congratulations!\n");
+    system("python get_fanpage_posts.py $fb_token",$ret);
+    echo("ret is $ret");
+  }
+?>
+<!DOCTYPE html>
+<html lang="tw">
+
+  <head>
+    <meta http-equiv="Content-Language" content="zh-tw" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" />
+
+    <title></title>
+
+    <link href="css/public.css" rel="stylesheet" type="text/css" />
+    <link href="css/icon.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <script src="js/jquery_v1.10.2/jquery-1.10.2.min.js" language="javascript" type="text/javascript" ></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
+
+    <script src="js/imgLiquid-min.js"></script>
+    <script src="js/public.js" language="javascript" type="text/javascript" ></script>
+
+
+    <script type="text/javascript">
+    $(function(){
+    $('body').scrollspy({ target: '#navbar-example' })
+    });
+    </script>
+
+
+    <script type="text/javascript">
+      $(function(){
+        $("ul>li>a").click(function(event) {
+          event.preventDefault();
+          var ref = $(this).attr("href");
+          $("body,html").animate({scrollTop: $(ref).offset().top },1000);
+        });
+        $('body').scrollspy({target: '#navbar-example'})
+      });
+      </script>
+
+  </head>
+
+  <body lang="zh-tw" >data-spy="scroll" data-target="#navbar-example">
+    <header id='header'>
+      <nav id="navbar-example" class="navbar navbar-default navbar-fixed-top">
+        <div class='container'>
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#top">首頁</a></li>
+                <li><a href="#event">遊戲</a></li>
+                <li><a href="#end">即時分析</a></li>
+              </ul>
+        </div>
+      </nav>
+    </header>
+
+    <div id='top'>
+      <div class='logo'>實況主所玩熱門遊戲</div>
+    </div>
+
+    <div id='banner'>
+        <div class='container'>
+          <div>實況主最近都在玩那些熱門遊戲?</div>
+          <h1>遊戲被那些實況主玩</h1>
+        </div>
+      </div>
+
+
+      <div id='event'>
+        <div class='container'>
+          <header>
+            <h2>熱門遊戲</h2>
+          </header>
+
+        <div class='events'>
+
+          <a class='event' href="discussion.php?g=尼爾：自動人形">
+            <img src='img/events/1.png' />
+            <span>尼爾：自動人形</span>
+          </a>
+          <a class='event' href="discussion.php?g=鐵拳7">
+            <img src='img/events/2.png' />
+            <span>鐵拳7</span>
+          </a>
+          <a class='event' href="discussion.php?g=DOTA2">
+            <img src='img/events/3.png' />
+            <span>DOTA2</span>
+          </a>
+          <a class='event' href="discussion.php?g=天堂2：革命">
+            <img src='img/events/4.png' />
+            <span>天堂2：革命</span>
+          </a>
+          <a class='event' href="discussion.php?g=十三號星期五">
+            <img src='img/events/5.png' />
+            <span>十三號星期五</span>
+          </a>
+          <a class='event' href="discussion.php?g=伊蘇8">
+            <img src='img/events/6.png' />
+            <span>伊蘇8</span>
+          </a>
+          <a class='event' href="discussion.php?g=崩壞3rd">
+            <img src='img/events/7.png' />
+            <span>崩壞3rd</span>
+          </a>
+          <a class='event' href="discussion.php?g=完全征服">
+            <img src='img/events/8.png' />
+            <span>完全征服</span>
+          </a>
+          <a class='event' href="discussion.php?g=爐石戰記">
+            <img src='img/events/9.png' />
+            <span>爐石戰記</span>
+          </a>
+          <a class='event' href="discussion.php?g=Dead Cells">
+            <img src='img/events/10.png' />
+            <span>Dead Cells</span>
+          </a>
+          <a class='event' href="discussion.php?g=星宮獵手">
+            <img src='img/events/11.png' />
+            <span>星宮獵手</span>
+          </a>
+          <a class='event' href="discussion.php?g=暴雪英霸">
+            <img src='img/events/12.png' />
+            <span>暴雪英霸</span>
+          </a>
+          <a class='event' href="discussion.php?g=鬥陣特攻">
+            <img src='img/events/13.png' />
+            <span>鬥陣特攻</span>
+          </a>
+          <a class='event' href="discussion.php?g=熾焰帝國2">
+            <img src='img/events/14.png' />
+            <span>熾焰帝國2</span>
+          </a>
+          <a class='event' href="discussion.php?g=陰陽師">
+            <img src='img/events/15.png' />
+            <span>陰陽師</span>
+          </a>
+          <a class='event' href="discussion.php?g=夢幻龍族">
+            <img src='img/events/16.png' />
+            <span>夢幻龍族</span>
+          </a>
+          <a class='event' href="discussion.php?g=傳說對決">
+            <img src='img/events/17.png' />
+            <span>傳說對決</span>
+          </a>
+          <a class='event' href="discussion.php?g=新世界的神">
+            <img src='img/events/18.png' />
+            <span>新世界的神</span>
+          </a>
+          <a class='event' href="discussion.php?g=闇影詩章">
+            <img src='img/events/19.png' />
+            <span>闇影詩章</span>
+          </a>
+          <a class='event' href="discussion.php?g=LOL">
+            <img src='img/events/20.png' />
+            <span>英雄聯盟</span>
+          </a>
+          <a class='event' href="discussion.php?g=仙劍奇俠傳">
+            <img src='img/events/21.png' />
+            <span>仙劍奇俠傳</span>
+          </a>
+          <a class='event' href="discussion.php?g=千姬大亂鬥">
+            <img src='img/events/22.png' />
+            <span>千姬大亂鬥</span>
+          </a>
+          <a class='event' href="discussion.php?g=陰屍路">
+            <img src='img/events/23.png' />
+            <span>陰屍路</span>
+          </a>
+          <a class='event' href="discussion.php?g=麥當勞報報">
+            <img src='img/events/24.png' />
+            <span>麥當勞報報</span>
+          </a>
+          <a class='event' href="discussion.php?g=技嘉科技">
+            <img src='img/events/25.png' />
+            <span>技嘉科技</span>
+          </a>
+          <a class='event' href="discussion.php?g=夢幻誅仙">
+            <img src='img/events/26.png' />
+            <span>夢幻誅仙</span>
+          </a>
+        </div>
+
+      </div>
+    </div>
+
+<br>
+<br>
+
+    <div id='end'>
+      <div class='container'>
+        <header>
+          <h2>即時分析</h2>
+
+          <form action="index.php" method="post">
+　           FB_token:
+            <input type="test" name="fb_token">
+　          <input type="submit" value="進行分析">
+          </form>
+        </header>
+      </div>
+    </div>
+
+  </body>
+</html>
